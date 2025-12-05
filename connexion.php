570717a66b4a79043b1007d984fcb321dff5c8f6
Haya-chb <haya.chaibi@hotmail.com>
2025-12-05 03:44:28 +0100

    form finito

--- connexion.php
+++ connexion.php
@@ -1,26 +1,47 @@
 <?php
-
+session_start();
 include('connect.php');
 
 
-echo`
+echo '<form action="connexion.php" method="post">';
 
-<form action="">
+echo '<div>';
+echo '<label for="login">Login :</label><br>';
+echo '<input type="text" id="login" name="login">';
+echo '</div>';
 
-<div>
-<label for="login">Login :</label><br>
-<input type="text" id="login" name="login">
-</div>
+echo '<div>';
+echo '<label for="pswd">Mot de passe :</label><br>';
+echo '<input type="password" id="pswd" name="pswd">';
+echo '</div> <br>';
 
-<div>
-<label for="pswd">Mot de passe :</label><br>
-<input type="password" id="pswd" name="pswd">
-</div>
+echo '<input type="submit" name="connexion" value="Connexion">';
 
-<input type="submit" value="Connexion">
-
-</form>`;
+echo '</form>';
 
 
+
+if (isset($_POST['connexion'])) {
+
+    $login = trim($_POST['login']);
+    $password = $_POST['pswd'];
+
+    $stmt = $db->prepare('SELECT id_utilisateur, login, password
+                          FROM utilisateur 
+                          WHERE login = :login');
+
+    $stmt->bindParam(':login', $login, PDO::PARAM_STR);
+    $stmt->execute();
+    $utilisateur = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($utilisateur && password_verify($password, $utilisateur['password'])) {
+
+        $_SESSION['id_utilisateur'] = $utilisateur['id_utilisateur'];
+        header('Location: index.php');
+        exit;
+
+    } else {
+        echo '<p>Login ou mot de passe incorrect.</p>';
+    }
+}
 ?>
-
--- identification.php
+++ identification.php
@@ -1,25 +1,23 @@
 <?php
 
-echo`<form action="identification.php" method="get">
-<input type="submit" value="Connexion" name="connexion">
-<input type="submit" value="Inscription" name="inscription">
-</form>`;
+include('connect.php');
+echo'<form action="identification.php" method="get">';
+echo'<input type="submit" value="Connexion" name="connexion">';
+echo'<input type="submit" value="Inscription" name="inscription">';
+echo'</form>';
 
 
 if (isset($_GET['connexion']) && $_GET['connexion'] === 'Connexion'){
 
 header('Location: connexion.php');
+exit;
 }
 
 
 elseif (isset($_GET['inscription']) && $_GET['inscription'] === 'Inscription') {
 
     header('Location: inscription.php');
+    exit;
 }
-
-
-
-
-
 
 ?>
