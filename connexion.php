e2bdf7b9927ba24ff52bc8222cdd208afe167dc7
Haya-chb <haya.chaibi@hotmail.com>
2025-12-05 02:06:09 +0100

    debut d'identification

--- connexion.php
+++ connexion.php
@@ -1,6 +1,26 @@
-
 <?php
 
-$db = new PDO ('mysql:host=localhost;dbname=nerd; port=3306','root','');
+include('connect.php');
+
+
+echo`
+
+<form action="">
+
+<div>
+<label for="login">Login :</label><br>
+<input type="text" id="login" name="login">
+</div>
+
+<div>
+<label for="pswd">Mot de passe :</label><br>
+<input type="password" id="pswd" name="pswd">
+</div>
+
+<input type="submit" value="Connexion">
+
+</form>`;
+
 
 ?>
+
